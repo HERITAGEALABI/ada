e71782d8d8003ff2f0d06f310c034e9567cc95bc
unknown <heritagealabi@gmail.com>
2020-05-02 15:06:38 +0000

    initial commit

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 let btnAdd = document.querySelector('#add');
 let btnSubtract = document.querySelector('#subtract');
-let input = document.querySelector('input');
+let input = document.querySelector('#input');
 
 btnAdd.addEventListener('click', () => {
 input.value = parseInt(input.value) + 1;
-document.getElementById('add').style.color = 'green';
-if(parseInt(input.value) >=0) {
-    document.getElementById('add').style.color = 'green';
+
+if(parseInt(input.value) >= 0) {
+    document.getElementById('input').style.color = 'green';
 }
 });
 
 btnSubtract.addEventListener('click',  () => {
     input.value = parseInt(input.value) - 1; 
-    document.getElementById('subtract').style.color = 'red';
-    if(parseInt(input.value) <=0) {
-        document.getElementById('subtract').style.color = 'red'
+   
+    if(parseInt(input.value) <= 0) {
+        document.getElementById('input').style.color = 'red'
     }
 });
 
